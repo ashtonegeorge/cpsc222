1698083b6207193ca0a74458655077e2081ad9e3
Ashton George <ashtongeorge17@gmail.com>
2026-01-21 09:09:27 -0500

    finish sudo counter script

--- snmpsudousage/script.pl
+++ snmpsudousage/script.pl
@@ -1,3 +1,19 @@
 #!/usr/bin/env perl
 
-print "Hello World!\n"
+my $sudoCount = 0;
+
+my $log = '/var/log/auth.log';
+
+open(my $fh, '<', $log)
+	or die "Cannot open $log: $!"; 
+
+while(my $line = <$fh>) {
+	if($line =~ /\bsudo:/) {
+		$sudoCount++;
+	}
+}
+
+close($fh);
+
+print"$sudoCount\n"
+
